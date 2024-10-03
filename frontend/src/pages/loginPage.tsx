596b83b75b9146ac74a43e9c7db02c805dc8b1b0
Meacronacer <sinnerscrown09@gmail.com>
2024-10-03 19:05:44 +0300

    test commit

--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,6 +1,6 @@
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 text-center lg:px-8">
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 text-center text-black lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           alt="Your Company"

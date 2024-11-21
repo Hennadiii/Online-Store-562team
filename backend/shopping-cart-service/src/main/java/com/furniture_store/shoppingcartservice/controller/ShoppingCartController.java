31ee0d3f9b45202e5cb0e922278c925ceec349d0
pavlobarabah <pavlobarabah@gmail.com>
2024-11-21 22:37:55 +0200

    почав роботу над контролером

---
+++ backend/shopping-cart-service/src/main/java/com/furniture_store/shoppingcartservice/controller/ShoppingCartController.java
@@ -0,0 +1,12 @@
+package com.furniture_store.shoppingcartservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/carts")
+public class ShoppingCartController {
+
+}

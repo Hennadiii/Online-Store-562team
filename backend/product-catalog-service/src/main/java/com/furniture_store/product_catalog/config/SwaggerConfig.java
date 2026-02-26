bd8e29f8d6a76e322b0a88c02c2864a936d2f631
Hennadiii <muhach2020@ukr.net>
2026-02-26 23:50:51 +0200

    added healthcheck

--- backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/config/SwaggerConfig.java
+++ backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.furniture_store.product_catalog.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,15 +13,7 @@ public class SwaggerConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Каталог товарів")
-                        .summary("Реалізує CRUD операції для роботи з товарами.")
-                        .description("API-документація мікросервісу Каталог товарів," +
-                                " що дозволяє" +
-                                " додавати, отримувати, модифікувати та видаляти товари.")
-                )
-                .addServersItem(new Server()
-                                .url("http://localhost:562")
-                                .description("Local development server")
-                        )
-                ;
+                        .summary("CRUD операції для роботи з товарами")
+                        .description("API-документація мікросервісу каталогу товарів"));
     }
 }

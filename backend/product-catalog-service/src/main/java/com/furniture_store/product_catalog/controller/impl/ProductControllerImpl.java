e9979e9c9424df0030b2cdc6087a18abb79dfcd2
Hennadiii <muhach2020@ukr.net>
2026-02-27 02:11:32 +0200

    added CORS settings

--- backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/controller/impl/ProductControllerImpl.java
+++ backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/controller/impl/ProductControllerImpl.java
@@ -15,6 +15,7 @@ import java.net.URI;
 /**
  * Контролер для керування операціями з продуктами.
  */
+@CrossOrigin(origins = "*") /*CORS */
 @RestController
 public class ProductControllerImpl implements ProductController {
 

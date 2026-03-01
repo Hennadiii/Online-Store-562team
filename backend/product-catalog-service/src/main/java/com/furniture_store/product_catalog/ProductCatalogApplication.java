f80d2e609d7b79d8016ee833ad96ae701196ff9d
Hennadiii <muhach2020@ukr.net>
2026-03-01 11:04:30 +0200

    fix:server url

--- backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/ProductCatalogApplication.java
+++ backend/product-catalog-service/src/main/java/com/furniture_store/product_catalog/ProductCatalogApplication.java
@@ -1,13 +1,23 @@
 package com.furniture_store.product_catalog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+    servers = {
+        // Добавляем адрес твоего бэкенда на Render (замени на свой!)
+        @Server(url = "https://online-store-562team.onrender.com", description = "Production Server"),
+        // Оставляем локальный сервер для тестов дома
+        @Server(url = "http://localhost:10000", description = "Local Server")
+    }
+)
 @SpringBootApplication
 public class ProductCatalogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProductCatalogApplication.class, args);
+    }
 
 }

4b626e370b65d46404b62a02b98316f23208a082
Hennadiii <muhach2020@ukr.net>
2026-03-13 21:24:39 +0200

    fix: remove OrderController interface, fix CORS duplicate, SSL WebClient

--- backend/order-management-service/src/main/java/com/furniture_store/order_management_service/OrderManagementServiceApplication.java
+++ backend/order-management-service/src/main/java/com/furniture_store/order_management_service/OrderManagementServiceApplication.java
@@ -2,29 +2,11 @@ package com.furniture_store.order_management_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OrderManagementServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderManagementServiceApplication.class, args);
-    }
-
-    // Глобальный CORS для продакшн-домена
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // для всех эндпоинтов
-                        .allowedOrigins("https://online-store-562team.vercel.app") // сюда домен фронта
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true); // если используешь токены или куки
-            }
-        };
     }
 }
